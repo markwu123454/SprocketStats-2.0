c7a431f88c6b5fe15fbfe488604e798ffe3ed6c1
mark <mark.wu123454@gmail.com>
2026-05-22 11:24:44 -0700

    changes

--- frontend/src/pages/AttendancePage.tsx
+++ frontend/src/pages/AttendancePage.tsx
@@ -1,3 +1,5 @@
 export default function AttendancePage() {
-    return <></>
+    return <div className="mx-auto px-4 py-8 flex flex-col gap-8 h-full">
+        <h1 className="text-2xl font-bold theme-h1-color">Attendance & Schedule</h1>
+    </div>
 }
