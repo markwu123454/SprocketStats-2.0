af1cf42f03c7650aaf00b4db90b069d89da1c9dc
mark <mark.wu123454@gmail.com>
2026-07-07 12:27:05 -0700

    added ag grid theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import {createRoot} from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./lib/agGridTheme";
 
 navigator.serviceWorker?.addEventListener('controllerchange', () => {
   window.location.reload();
